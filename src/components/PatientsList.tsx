ccd0dc692bfe47528bd8efe81ef1c55ee8cff97f
Jair <jairaceves56@gmail.com>
2024-04-05 15:57:01 -0600

    Guardamos en el Sate

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,5 +1,11 @@
+import { usePatientStore } from "../store"
 
 export const PatientsList = () => {
+
+  const patients = usePatientStore(state => state.patients)
+  console.log(patients);
+  
+
   return (
     <div>PatientsList</div>
   )
