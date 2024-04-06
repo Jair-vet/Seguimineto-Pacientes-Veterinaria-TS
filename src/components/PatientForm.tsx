145021cc08b74d2bb3676674ba47a9876f9e3513
Jair <jairaceves56@gmail.com>
2024-04-05 18:10:17 -0600

    llenamos el formulario con los datos del paciente

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -2,13 +2,25 @@ import { useForm } from "react-hook-form"
 import { Error } from "./Error"
 import { DraftPatient } from "../types"
 import { usePatientStore } from "../store"
-
+import { useEffect } from "react"
 
 
 export const PatientForm = () => {
 
-    const { addPatient } = usePatientStore()
-    const { register, handleSubmit, formState: {errors}, reset } = useForm<DraftPatient>()
+    const { addPatient, activeId, patients } = usePatientStore()
+    const { register, handleSubmit, setValue, formState: {errors}, reset } = useForm<DraftPatient>()
+
+    useEffect(() => {
+        if(activeId) {
+            const activePatient = patients.filter( patient =>  patient.id === activeId)[0]
+            setValue('name', activePatient.name)
+            setValue('caretaker', activePatient.caretaker)
+            setValue('date', activePatient.date)
+            setValue('email', activePatient.email)
+            setValue('symptoms', activePatient.symptoms)
+        }
+    },[activeId])
+
     const registerPatient = (data : DraftPatient) => {
         addPatient(data)
         // Resetear
